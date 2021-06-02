76be3040da8f4438d950cb2fc569060f5050a268
USAMAWIZARD <usamawizard@gmail.com>
2021-06-02 10:57:03 +0530

    sdf

--- views/dataSenderProcessor.js
+++ views/dataSenderProcessor.js
@@ -3,13 +3,11 @@
 class DataSenderProcesor extends AudioWorkletProcessor {
   constructor(){
     super()
-  this.recordingBuffer = new Float32Array(8000  * 20);  // store max 20 sec audio
-  this.recordingBufferOffset = 0; 
   }
-  process (inputs, outputs, parameters) {
+  process (inputs, outputs, parameters) { // process function will be called automatically when  audio data is available to be processed 
      var buff=inputs[0]
      var input = inputs[0];            //input[0] is microphone audio data
-     const output = outputs[0]        // process function will be called automatically when  audio data is available to be processed 
+     const output = outputs[0]        
 
     //  console.log(buff)
       this.port.postMessage(
